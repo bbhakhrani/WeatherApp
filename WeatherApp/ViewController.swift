20dcedd1ca3913f483c75629236d4d382a8317c5
Bhakhrani <Bhakhrani@Bhakhranis-MacBook-Pro.local>
2017-12-16 20:39:49 -0800

    Used a closure to access the data from the request and update the relevent labels accordingly. I also added location, and embedded in a navigation and tab view

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -7,27 +7,62 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var tempLabel: UILabel!
-    
     @IBOutlet weak var descLabel: UILabel!
-    
     @IBOutlet weak var cityLabel: UILabel!
+//    @IBOutlet weak var tempLabel: UILabel!
+//    
+//    @IBOutlet weak var descLabel: UILabel!
+//
+//    @IBOutlet weak var cityLabel: UILabel!
     
-    @IBAction func setCityClicked(_ sender: UIButton) {
-        print("City button clicked")
+    
+    
+    
+    func kelvinToFarenheit(kelvinTemp: Double) -> Double {
+        return ((kelvinTemp * 1.8) - 459.67)
     }
     
+    func updateData(weatherData: [String: Any]) {
+        if var temp = weatherData["temp"] as? Double {
+            temp = kelvinToFarenheit(kelvinTemp: temp)
+            self.tempLabel.text = String(format: "%.2f", temp) + " \u{00B0}F"
+            self.tempLabel.sizeToFit()
+        }
+        if let desc = weatherData["desc"] as? String {
+            self.descLabel.text = desc
+        }
+        print("updated")
+    }
     
+    let manager = CLLocationManager()
     
-
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("error")
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        let weather = Weather()
+        weather.getWeatherData(lat: String(location.coordinate.latitude), lon: String(location.coordinate.longitude), closure: updateData)
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let weather = Weather(currLat: "33.7592", currLon: "-117.9897")
+        let weather = Weather()
+        weather.getFiveDayWeather(lat: "33.76", lon: "-117.99")
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        manager.requestLocation()
+        manager.startMonitoringSignificantLocationChanges()
+        
+        //manager.desiredAccuracy left as default (best)
     }
 
     override func didReceiveMemoryWarning() {
--- WeatherApp/Weather.swift
+++ WeatherApp/Weather.swift
@@ -9,97 +9,52 @@
 import Foundation
 
 class Weather{
-    private let openWeatherMapBaseURL = "http://api.openweathermap.org/data/2.5/weather"
+    private let openWeatherMapBaseURL = "http://api.openweathermap.org/data/2.5/"
     private let openWeatherMapAPIKey = "5b986aec3061a6ebe330affa40fbac0a"
-    private var lat: String
-    private var lon: String
     private var description: String
     private var temp: Double
     private var tempMin: Double
     private var tempMax: Double
-    init(currLat:String, currLon:String){
-        lat = currLat
-        lon = currLon
+    
+    init(){
         description = ""
         temp = 0
         tempMin = 0
         tempMax = 0
-        getWeatherData()
     }
     
-    func setLocation(currLat:String, currLon:String) {
-        lat = currLat
-        lon = currLon
-        getWeatherData()
-    }
-    
-    func getTemp() -> Double? {
-        if temp == 0 && tempMin == 0 && tempMax == 0 {
-            return nil
-        } else {
-            return ((temp-273.15)*1.8 + 32)
-        }
-    }
-    
-    func getMinTemp() -> Double? {
-        if temp == 0 && tempMin == 0 && tempMax == 0 {
-            return nil
-        } else {
-            return ((tempMin-273.15)*1.8 + 32)
-        }
-    }
-    
-    func getMaxTemp() -> Double? {
-        if temp == 0 && tempMin == 0 && tempMax == 0 {
-            return nil
-        } else {
-            return ((tempMax-273.15)*1.8 + 32)
-        }
-    }
-    
-    func getDescription() -> String? {
-        if description == "" {
-            return nil
-        } else {
-            return description
-        }
-    }
-    
-    private func getWeatherData(){
-        let weatherRequestURLString = "\(openWeatherMapBaseURL)?lat=\(lat)&lon=\(lon)&appid=\(openWeatherMapAPIKey)"
+    func getWeatherData(lat:String, lon:String, closure: @escaping ([String: Any]) ->()) {
+        let weatherRequestURLString = "\(openWeatherMapBaseURL)weather?lat=\(lat)&lon=\(lon)&appid=\(openWeatherMapAPIKey)"
         let weatherRequestURL = URL(string: weatherRequestURLString)!
         let session = URLSession.shared.dataTask(with: weatherRequestURL){(data, response, error) in
             if let error = error { //Error when getting data
                 print("Error: \n\(error)")
             }
             else { //Success
-                //print("Raw Data:\n\(data)")
-                //let dataString = String(data: data!, encoding: String.Encoding.utf8)
-                //print("Readable Data: \n\(dataString!)")
+                var weatherData = [String:Any]()
+                
                 let json = try? JSONSerialization.jsonObject(with: data!, options: [])
-                //print("JSON: \n\(json)\n")
                 
                 if let dictionary = json as? [String: Any] {
-                    print(dictionary)
                     if let main = dictionary["main"] as? [String: Any] {
                         if let tmpTemp = main["temp"] as? Double {
                             self.temp = tmpTemp
-                            print(self.temp)
+                            weatherData["temp"] = self.temp
                         }
                         if let tmpTempMin = main["temp_min"] as? Double {
                             self.tempMin = tmpTempMin
-                            print(self.tempMin)
+                            weatherData["tempMin"] = self.tempMin
                         }
                         if let tmpTempMax = main["temp_max"] as? Double {
                             self.tempMax = tmpTempMax
-                            print(self.tempMax)
+                            weatherData["tempMax"] = self.tempMax
                         }
                     } else {
                         print("main not a sub dictionary")
                     }
                     if let weatherArray = dictionary["weather"] as? [[String: Any]] {
                         if let description = weatherArray[0]["description"] as? String {
-                            print(description)
+                            weatherData["desc"] = description
                         }
                         
                     } else {
@@ -108,11 +63,27 @@ class Weather{
                 } else {
                     print("JSON invalid")
                 }
+                closure(weatherData)
             }
         }
         
         session.resume()
         
+    }
+    
+    func getFiveDayWeather(lat:String, lon:String) {
+        let weatherRequestURLString = "\(openWeatherMapBaseURL)forecast?lat=\(lat)&lon=\(lon)&appid=\(openWeatherMapAPIKey)"
+        let weatherRequestURL = URL(string: weatherRequestURLString)!
+        let session = URLSession.shared.dataTask(with: weatherRequestURL){(data, response, error) in
+            if let error = error { //Error when getting data
+                print("Error: \n\(error)")
+            }
+            else { //Success
+                let json = try? JSONSerialization.jsonObject(with: data!, options: [])
+                print(json!)
+            }
+        }
+        session.resume()
     }
     
 }
